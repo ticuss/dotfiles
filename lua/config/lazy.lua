495327333c5ad683642623cf0ccd2e31ec0b3bd6
Constantin Bogdanas <ticu369@gmail.com>
2024-05-22 18:11:34 +0200

    Add go support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,6 +14,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.dap.core" },
